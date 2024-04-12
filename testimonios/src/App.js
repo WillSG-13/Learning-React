48553f44ce853cd38a53b78442ebd822735fecc3
Wilmar Sequeira Guevara <80023763+WillSG-13@users.noreply.github.com>
2024-04-12 02:50:26 -0600

    se termino el de testimonios
    se termino el de testimonio y se creo el de contador de clicks

--- testimonios/src/App.js
+++ testimonios/src/App.js
@@ -3,9 +3,43 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      <div className='contenedor-Principal'>
+      <div className='contenedor-principal'>
         <h1> Esto es lo que dice esta gente que deja sus reseñas </h1>
-        <Testimonio />
+        <Testimonio
+          nombre='Familia SG'
+          pais='Costa Rica'
+          imagen='fam'
+          cargo='Mamahuevos'
+          empresa='Casa'
+          alt='Foto familia'
+          testimonio='Mobbin like a motherfucker, stuck
+          Can you put your middle finger out the window
+          Gettin fucked off liquor, gettin loaded, cant control it
+          Nigga, pass me the blunt and let me roll it
+          You get the bozack to the ho-stack, thats for sure, black
+          So be a mack and keep your dough fat
+          And tell me, what does it take to be a G?
+          I started with a quarter ounce, soon bounced to a key
+          You gotta watch your back, stay strapped, be alert
+          Started as a young motherfucker doin dirt
+          And now Im in the rap game, like the crack game, I got enemies
+          Cant pretend to see my friends are not my enemies'/>
+           <Testimonio
+          alt='foto sacha'
+          nombre='Sacha'
+          pais='Costa Rica'
+          imagen='sacha'
+          cargo='Perra Necia'
+          empresa='Casa '
+          testimonio='Mobbin like a motherfucker'/>
+           <Testimonio
+          nombre='Choque'
+          alt='foto choque'
+          pais='Costa Rica'
+          imagen='choque'
+          cargo='La doña'
+          empresa='Calle '
+          testimonio='esta doña pego el carro contra un peaje'/>
       </div>
     </div>
   );
--- testimonios/src/componentes/Testimonio.js
+++ testimonios/src/componentes/Testimonio.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img
         className='imagen-testimonio'
-        src={require('../imagenes/fam.jpg')}
-        alt='Foto Familia' />
+        src={require(`../imagenes/testimonio-${props.imagen}.jpg`)}
+        alt={props.alt}/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Familia Sequeira Guevara en Puntarenas</p>
-        <p className='cargo-testimonio'>Monton de mamaguevos</p>
-        <p className='texto-testimonio'>Mobbin' like a motherfucker, stuck
-          Can you put your middle finger out the window
-          Gettin' fucked off liquor, gettin' loaded, can't control it
-          Nigga, pass me the blunt and let me roll it
-          You get the bozack to the ho-stack, that's for sure, black
-          So be a mack and keep your dough fat
-          And tell me, what does it take to be a G?
-          I started with a quarter ounce, soon bounced to a key
-          You gotta watch your back, stay strapped, be alert
-          Started as a young motherfucker doin' dirt
-          And now I'm in the rap game, like the crack game, I got enemies
-          Can't pretend to see my friends are not my enemies </p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
     </div>
   );
